1dded3f56f8cb1bb8bda8836b8a812e979c0c98a
JDPescosta <jamesdpescosta@gmail.com>
2019-12-13 10:46:15 -0800

    Added computerPlay func to randomly gen a string of rock, paper or scissors

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('pickles');
+function computerPlay() {
+
+    let answer;
+    let x = Math.floor(Math.random() * 3)
+    
+    if (x == 0){
+        answer = 'rock'
+    }
+    else if (x == 1){
+        answer = 'paper'
+    }
+    else {
+        answer = 'scissors'
+    }
+    return answer;
+}
+
